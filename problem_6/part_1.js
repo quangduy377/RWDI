54919b66452af01d8c79d6b80a72718c55a64dd5
Duy <zigs123321@gmail.com>
2022-12-06 10:32:43 -0600

    Finish problem 6

---
+++ problem_6/part_1.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+
+
+const data = fs.readFile('data.txt','utf8',(err,data)=>{
+    const letters = data.trim().replace(/(\r)/gm, "").split('')
+    let arr = []
+    let endFlag = false
+    let count = 0
+    for(let i=0;i<letters.length;i++){
+        for(let j=i;j<letters.length;j++){
+            let result = arr.find(el=>el == letters[j])
+            //found a letter that doesn't exist in the array
+            if(!result){
+                arr.push(letters[j])
+            }
+            //dupplicated letter while the length of the array doesn't equal to 4
+            else{
+                arr=[]
+                break;
+            }
+            //found the first 4-letter pattern
+            if(arr.length==4){
+                count=j+1
+                endFlag = true
+                break;
+            }
+        }
+        if(endFlag){
+            break
+        }
+    }
+    console.log(arr)
+    console.log(count)
+})
